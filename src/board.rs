5c5a811859095a832ab56aefccfc7805f5810f13
John Foley <john@hisfoleyness.com>
2015-04-13 19:42:59 -0700

    uncovering tiles

--- src/board.rs
+++ src/board.rs
@@ -1,54 +1,73 @@
 extern crate rand;
 use self::rand::Rng;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 struct Point {
     x: usize,
     y: usize,
 }
 
 #[derive(Copy, Clone)]
-pub struct Tile {
+pub struct Cell {
     mine: bool,
     visible: bool,
 }
 
 pub struct Board {
-    points: [[Tile; 10]; 10]
+    cells: Vec<Vec<Cell>>
 }
 
 impl Board {
-    fn new(mines: Vec<Point>) -> Board {
-        let mut board = Board{points: [[Tile{mine: false, visible: false} ;10]; 10]};
+    fn new(size: usize, mines: Vec<Point>) -> Board {
+        let mut rows = Vec::with_capacity(size);
+        for y in 0..size {
+            let mut cells = Vec::with_capacity(size);
 
-        for mine in mines.iter() {
-            board.points[mine.x][mine.y].mine = true;
+            for x in 0..size {
+                cells.push(Cell{mine: false, visible: false});
+            }
+
+            rows.push(cells);
         }
 
-        board
+        for mine in mines.iter() {
+            rows[mine.x][mine.y].mine = true;
+        }
+
+        Board{cells: rows}
     }
 
     fn uncover(&mut self, point: Point) -> bool {
-        self.points[point.x][point.y].visible = true;
-        self.points[point.x][point.y].mine
+        self.cells[point.x][point.y].visible = true;
+        self.cells[point.x][point.y].mine
+    }
+
+    fn cell_at(&self, point: Point) -> Cell {
+        self.cells[point.x][point.y]
     }
 }
 
 #[test]
 fn creating_a_board() {
-    let mines = vec![Point{x: 1, y: 1}];
-    let board = Board::new(mines);
+    let mines = vec![Point{x: 0, y: 0}];
+    let board = Board::new(1, mines);
 
-    assert_eq!(board.points[1][1].visible, false);
-    assert_eq!(board.points[1][1].mine, true);
+    assert_eq!(board.cell_at(Point{x: 0, y: 0}).visible, false);
+    assert_eq!(board.cell_at(Point{x: 0, y: 0}).mine, true);
 }
 
 #[test]
 fn uncovering_tiles() {
-    let mines = vec![Point{x: 1, y: 1}];
-    let mut board = Board::new(mines);
+    let mines = vec![
+        Point{x: 0, y: 0},
+        Point{x: 1, y: 1}
+    ];
+    let mut board = Board::new(2, mines);
 
-    let is_mine = board.uncover(Point{x: 2, y: 1});
+    let is_mine = board.uncover(Point{x: 1, y: 0});
     assert_eq!(is_mine, false);
-    assert_eq!(board.points[2][1].visible, true)
+    assert_eq!(board.cell_at(Point{x: 0, y: 0}).visible, false);
+    assert_eq!(board.cell_at(Point{x: 1, y: 0}).visible, true);
+    assert_eq!(board.cell_at(Point{x: 1, y: 1}).visible, false);
+    assert_eq!(board.cell_at(Point{x: 1, y: 1}).visible, false)
 }
